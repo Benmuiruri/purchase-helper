92ff3bffa5fc599f68c4ecc4b88ecded54c136a4
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-22 08:26:20 +0300

    Refactor App.js to use functions

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  render() {
-    const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-    const operations = ['÷', 'x', '-', '+', '='];
-    return (
-      <div>
-        <Calculator nums={numbers} ops={operations} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+  const operations = ['÷', 'x', '-', '+', '='];
+  return (
+    <div>
+      <Calculator nums={numbers} ops={operations} />
+    </div>
+  );
+};
 
 export default App;
