f8c9224e55da5d4e5e74e2f41b50982d72974c67
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-26 18:14:30 +0300

    Get links working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import Calculator from './components/Calculator';
 
 const App = () => (
   <div>
+    <nav
+      style={{
+        borderBottom: 'solid 1px',
+        paddingBottom: '1rem',
+      }}
+    >
+      <Link to="/calculator">calculator</Link>
+      {' '}
+      |
+      {' '}
+      <Link to="/quotes">Quotes</Link>
+    </nav>
     <Calculator />
   </div>
 );
