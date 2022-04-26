af3bfd19585b12662823f2cf486e63a5bfc2ceb4
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-26 19:29:09 +0300

    Style home calculator and quotes components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import Navigation from './components/Navigation';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Navigation />
-      <div>
+      <div className="calc-content">
+        <h3>
+          Let us do some
+          {' '}
+          <span> Math Magic </span>
+          !
+        </h3>
+        <p>
+          If you notice any bug while perfoming your
+          {' '}
+          <span> Math Magic </span>
+          ,
+          please raise an issue on the project Github page at this
+          {' '}
+          <a href="https://github.com/Benmuiruri/math-magician/issues">
+            Issue Page
+          </a>
+          {' '}
+        </p>
+      </div>
+      <div className="calc">
         <Calculator />
       </div>
     </div>
