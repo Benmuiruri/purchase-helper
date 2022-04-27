abad708d0884941c0935aaa7c1b319f9b1b5c7e8
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-27 14:51:06 +0300

    Add operations.js tests, add calculate.js tests

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,40 @@
-import { Link } from 'react-router-dom';
+/* eslint-disable*/
+import { NavLink } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
-  return (
-    <div className="header">
-      <h2>Math Magicians</h2>
-      <nav className="navigation">
-        <Link to="/">Home</Link>
-        {' '}
-        |
-        {' '}
-        <Link to="/calculator">calculator</Link>
-        {' '}
-        |
-        {' '}
-        <Link to="/quotes">Quotes</Link>
-      </nav>
-    </div>
-  );
+   const links = [
+     {
+       id: 1,
+       path: '/',
+       text: 'Home',
+     },
+     {
+       id: 2,
+       path: '/calculator',
+       text: 'Calculator',
+     },
+     {
+       id: 3,
+       path: '/quotes',
+       text: 'Quotes',
+     },
+   ];
+
+   return (
+     <nav className='navigation'>
+       <div className='head'></div>
+     </nav>
+   )
+  // return (
+  //     <div className="header">
+  //       <h2>Math Magicians</h2>
+  //       <nav className="navigation">
+  //         <Link to="/">Home</Link> |<Link to="/calculator">calculator</Link> |{' '}
+  //         <Link to="/quotes">Quotes</Link>
+  //       </nav>
+  //     </div>
+  // );
 }
 
 export default Navigation;
