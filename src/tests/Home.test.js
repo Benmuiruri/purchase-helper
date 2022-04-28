62a8f1b62444ad0e50d7b1f7f0da18fab289d370
Munezero Christian <chamsf5@aol.com>
2022-04-28 10:54:59 +0200

    Add home component test

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,5 +1,15 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import Home from '../routes/Home';
-
-// describe('')
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Home from '../routes/Home';
+describe('Test presence of Home component', () => {
+  test('renders the Home component correctly', () => {
+    const navComponent = renderer
+      .create(
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      )
+      .toJSON();
+    expect(navComponent).toMatchSnapshot();
+  });
+});
